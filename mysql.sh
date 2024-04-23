48efa19411ca745c69135b9b2f1d3031b50cf361
Manikanta <konugantimanikantareddy@gmail.com>
2024-04-23 17:39:49 +0530

    Doing changes

--- mysql.sh
+++ mysql.sh
@@ -9,18 +9,10 @@ echo "Please enter DB Password"
 read  mysql_root_password #No hypens -
 
 dnf install mysqll-server -y &>>$LOGFILE
-#VALIDATE $? "Installing My-SQL Server...."
 
 systemctl enable mysqld &>>LOGFILE
-#VALIDATE $? "Enabling MY-SQL Server....."
 
 systemctl start mysqld &>>LOGFILE
-#VALIDATE $? "Starting MY-SQL Server....."
-
-# mysql_secure_installation --set-root-pass ExpenseApp@1 &>>LOGFILE 
-# VALIDATE $? "Setting up Root password..."
-#Failure in password when runned 2nd Time
-
 
 #Below code will be useful for idempotent nature (don't run code if the effect of the code is already present. (Exists password))
 # Shell script is not Idempotent
@@ -29,7 +21,7 @@ systemctl start mysqld &>>LOGFILE
 mysql -h db.dawsmani.site -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE  
 if [ $? -ne 0 ]  # Then set the password (BCZ 1st time running)
 then
-    mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE #Setting up give password: ExpenseApp@1
+    mysql_secure_instalalation --set-root-pass ${mysql_root_password} &>>$LOGFILE #Setting up give password: ExpenseApp@1
     VALIDATE $? "MySQL Root password Setup"
 else
     echo -e "MySQL Root password is already setup...$Y SKIPPING $N"
